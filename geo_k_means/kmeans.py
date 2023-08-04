43ac581bb15fbe7193558c578a2f17ede88940ef
azevedo <azevedoantoniocicero@gmail.com>
2023-08-03 21:18:58 -0300

    funçao de distancia para n-dimensoes

--- geo_k_means/kmeans.py
+++ geo_k_means/kmeans.py
@@ -3,15 +3,13 @@ from math import sqrt
 import numpy as np
 
 
-def euclidean_distance(
-    ponto1: list[int, int], ponto2: list[int, int]
-) -> float:
+def euclidean_distance(ponto1: list[int], ponto2: list[int]) -> float:
     """
-    Calcula a distância Euclidiana entre dois pontos no plano cartesiano.
+    Calcula a distância entre dois pontos no espaço euclidiano n-dimensional.
 
     Parameters:
-        ponto1: primeiro ponto do plano cartesiano
-        ponto2: segundo ponto do plano cartesiano
+        ponto1: lista de coordenadas do primeiro ponto
+        ponto2: lista de coordenadas do segundo ponto
     Returns:
         Um float que represeta a distância entre os dois pontos.
 
@@ -19,9 +17,17 @@ def euclidean_distance(
         >>> euclidean_distance([2, 1], [6, 4])
         5.0
 
-        >>> euclidean_distance([0, 0], [20, 15])
-        25.0
+        >>> euclidean_distance([-2, 0, 1], [0, 2, 2])
+        3.0
 
+        >>> euclidean_distance([2, 1], [6, 4, 3])
+        Traceback (most recent call last):
+        ...
+        ValueError: Os pontos devem ter a mesma dimensão.
     """
+    if len(ponto1) != len(ponto2):
+        raise ValueError('Os pontos devem ter a mesma dimensão.')
 
-    return sqrt((ponto1[0] - ponto2[0]) ** 2 + (ponto1[1] - ponto2[1]) ** 2)
+    distance = sum((x - y) ** 2 for x, y in zip(ponto1, ponto2)) ** 0.5
+
+    return distance
