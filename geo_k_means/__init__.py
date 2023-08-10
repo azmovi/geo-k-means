0e8cd9bb51b21385f75468c83717fcb1765d9b7e
azevedo <azevedoantoniocicero@gmail.com>
2023-08-10 13:16:40 -0300

    adicionando umas descrições de execução

--- geo_k_means/__init__.py
+++ geo_k_means/__init__.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 
-from geo_k_means.kmeans import KMeans
+from geo_k_means.kmeans import KMeans # usando o taskipy 
+# from kmeans import KMeans #rodando o codigo normalmente
 
 
 def duas_dimensoes():
